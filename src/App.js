17381c10075fc93e8c4427d2419ca7b6f1d6d703
Ruvini Senevirathna <ruviniruvi@gmail.com>
2021-01-22 17:08:17 -0500

    Navbar files added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Anime from './pages/Anime';
+import Books from './pages/Books';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/anime' component={Anime} />
+          <Route path='/books' component={Books} />
+        </Switch>
+      </Router>
+    </>
   );
 }
 
---
+++ src/pages/Anime.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Anime() {
+  return (
+    <div className='anime'>
+      <h1>Anime</h1>
+    </div>
+  );
+}
+
+export default Anime;
