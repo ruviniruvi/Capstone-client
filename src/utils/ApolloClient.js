0abdbf973e391fdbef6ff66cd96f9e78df2a4e9c
Gisellen <giselle.domingo.x@gmail.com>
2021-01-23 21:21:05 -0500

    Modified Files

--- src/utils/ApolloClient.js
+++ src/utils/ApolloClient.js
@@ -1,32 +1,10 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
-import { gql } from "@apollo/client";
 // obtaining connection to the server and
 // create the access to server
 const client = new ApolloClient({
   uri: "https://capstone-ttp1.herokuapp.com/graphql",
   cache: new InMemoryCache(),
 });
-
-// test = () => {client.query({
-//   query: gql`
-//     {
-//       listings {
-//         id
-//         Title
-//         Notes
-//         Started_At
-//         Finished_At
-//         filter {
-//           filter_Name
-//         }
-//         tags {
-//           Tags
-//         }
-//       }
-//     }
-//   `,
-// })
-// .then(result=>console.log(result))}
 
 //export for the rest of the website to access
 export default client;
