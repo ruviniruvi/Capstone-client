5e2fec682af0d7955e4ae2f1ac75c1f13a340056
Gisellen <giselle.domingo.x@gmail.com>
2021-01-24 11:26:29 -0500

    Fixed formatting

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,10 @@
 import React from "react";
-import { useQuery, gql } from "@apollo/client";
-import { GET_POSTS } from "../utils/Queries";
 
 function Home() {
-    const { loading, error, data } = useQuery(GET_POSTS);
 
   return (
     <div className="home">
       <h1>USER</h1>
-      {console.log(data)} 
     </div>
   );
 }
