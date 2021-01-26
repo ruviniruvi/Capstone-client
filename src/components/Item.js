2f516278d22270e32c6937dbfeeca55f98a9ffe5
Gisellen <giselle.domingo.x@gmail.com>
2021-01-25 19:06:40 -0500

    Added delete function

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,19 +1,76 @@
-import React from 'react'
-import '../App.css'
+// import React from 'react'
+// import '../App.css'
 
-export default function Item(props) {
-    let item = props.item
-    if(item.length === 1){
-        return(
-            <div className="listing" key={item.id}>
-            <img src={item[0].Image.name} height="400" />
+// export default function Item(props) {
+//     let item = props.item
+//     if(item.length === 1){
+//         return(
+//             <div className="listing" key={item.id}>
+//             <img src={item[0].Image.name} height="400" />
+//         </div>
+//         )
+//     } else {
+//         return (
+//             <div className="listing" key={item.id}>
+//                 <img src={item.Image.name} height="400" />
+//             </div>
+//         )
+//     }
+// }
+
+import React, { Component } from "react";
+import "../App.css";
+import Modal from "./Modal";
+
+export default class Item extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      item: props.item,
+      isOpen: false,
+    };
+  }
+
+  toggleModal = () => {
+    this.setState({
+      isOpen: !this.state.isOpen,
+    });
+    console.log(this.state.item)
+  };
+
+  render() {
+    if (this.state.item.length === 1) {
+      return (
+        <div className="listing" key={this.state.item.id}>
+          <button onClick={this.toggleModal}>
+            <img src={this.state.item[0].Images} height="400" />
+          </button>
+
+          <Modal
+            show={this.state.isOpen}
+            onClose={this.toggleModal}
+            id={this.state.item.id}
+          >
+            {this.state.item[0].Notes}
+          </Modal>
         </div>
-        )
+      );
     } else {
-        return (
-            <div className="listing" key={item.id}>
-                <img src={item.Image.name} height="400" />
-            </div>
-        )
+      return (
+        <div className="listing" key={this.state.item.id}>
+          <button className="item-button" onClick={this.toggleModal}>
+            <img src={this.state.item.Images} height="400" />
+          </button>
+          <Modal
+            show={this.state.isOpen}
+            onClose={this.toggleModal}
+            id={this.state.item.id}
+          >
+            {this.state.item.Notes}
+          </Modal>
+        </div>
+      );
     }
+  }
 }
